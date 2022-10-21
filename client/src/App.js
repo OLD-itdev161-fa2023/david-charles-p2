b4cb12edefc117ba588ac075a2ace7ef73a7c60a
David Charles <charled1@gmatc.matc.edu>
2022-10-21 15:23:02 -0500

    Completed Activity 7

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 //import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
@@ -33,10 +35,10 @@ class App extends React.Component {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/register">Register</Link>
+              <Link to="/Register">Register</Link>
             </li>
             <li>
-              <Link to="/login">Login</Link>
+              <Link to="/Login">Login</Link>
             </li>
           </ul>
           </header>
@@ -45,12 +47,8 @@ class App extends React.Component {
               {this.state.data}
             </Route>
             <Switch>
-              <Route path="/register">
-                Register
-              </Route>
-              <Route path="Login">
-                Login
-              </Route>
+              <Route exact path="/Register" component={Register}/>
+              <Route exact path="/Login" component = {Login}/>
             </Switch>
           </main>
           
