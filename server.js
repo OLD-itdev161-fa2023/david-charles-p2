d787af65613b1735ae05a7c6a84f2947eb5024b7
David Charles <charled1@gmatc.matc.edu>
2022-11-03 13:13:18 -0500

    created post endpoint

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import auth from './middleware/auth';
+import Post from './models/Post';
 
 
 
@@ -129,6 +130,50 @@ app.post(
                 //Generate and return a JWT token
                 returnToken(user, res);
             } catch (error) {
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
+
+// Post endpoints
+
+app.post(
+    '/api/posts',
+    [
+        auth,
+        [
+            check('title', 'Title text is required')
+                .not()
+                .isEmpty(),
+            check('body', 'Body text is required')
+            .not()
+            .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({ errors: errors.array() });
+        } else {
+            const { title, body } = req.body;
+            try {
+                //Get the user who created the post
+                const user = await User.findById(req.user.id);
+
+                //Create a new post
+                const  post = new Post({
+                    user: user.id,
+                    title: title,
+                    body: body
+                });
+
+                //Save to the db and return
+                await post.save();
+
+                res.json(post);
+            } catch (error) {
+                console.error(error);
                 res.status(500).send('Server error');
             }
         }
